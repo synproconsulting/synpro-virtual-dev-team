6108339ad8adcf037af1e45fd0911ec42c8934c0
synproconsulting <synproconsulting@gmail.com>
2026-04-24 19:07:40 -0500

    feat(sdt1-30): implement manual uat deployment interface with service selec

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -3,8 +3,14 @@
 from .email_notifications import EmailNotificationService
 
 __all__ = ["EmailNotificationService"]
-from src.auth.manual_uat_deployment import ManualUATDeployment
-from src.auth.manual_uat_deployment import ServiceConfig
-from src.auth.manual_uat_deployment import ServiceType
-from src.auth.manual_uat_deployment import DeploymentEnvironment
-from src.auth.manual_uat_deployment import DeploymentProvider
+from src.auth.feature_brief_ui import FeatureBriefUI
+from src.auth.feature_brief_ui import FeatureBrief
+from src.auth.feature_brief_ui import Priority
+from src.auth.feature_brief_ui import FeatureStatus
+from src.auth.dependency_graph import DependencyGraph
+from src.auth.dependency_graph import DependencyVisualizer
+from src.auth.deployment_interface import DeploymentInterface
+from src.auth.deployment_interface import DeploymentConfig
+from src.auth.deployment_interface import DeploymentResult
+from src.auth.deployment_interface import DeploymentEnvironment
+from src.auth.deployment_interface import ServiceType
